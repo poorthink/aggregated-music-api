01ae8dcdc7c74774370b297b621dd63e1cd70825
thc <17876972635@163.com>
2025-02-02 20:39:03 +0800

    Add request.ts and constants/index.ts in spotify package

---
+++ spotify/constants/index.ts
@@ -0,0 +1,4 @@
+export const SPOTIFY = {
+  BASE_URL: "https://api.spotify.com",
+  AUTH_URL: "https://accounts.spotify.com",
+};
---
+++ spotify/utils/request.ts
@@ -0,0 +1,35 @@
+import { createAxios } from "@ama/utils";
+import { SPOTIFY } from "~/constants";
+
+export async function request<T = any>(
+  module: any,
+  options?: {
+    baseURL?: string;
+    token?: string;
+  }
+) {
+  const config = module;
+
+  const instance = createAxios(
+    {
+      baseURL: options?.baseURL || SPOTIFY.BASE_URL,
+    },
+    (config) => {
+      if (options && options.token) {
+        config.headers.Authorization = `Bearer ${options.token}`;
+      }
+      return config;
+    },
+    (response) => response
+  );
+
+  try {
+    const response = await instance(config);
+    return response.data as T;
+  } catch (err: any) {
+    if (err.name === "AxiosError") {
+      return err.response.data;
+    }
+    throw err;
+  }
+}
